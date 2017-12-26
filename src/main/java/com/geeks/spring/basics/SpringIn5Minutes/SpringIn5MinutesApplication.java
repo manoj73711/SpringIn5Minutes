beec9507d2db7d79b4d0364e310541723f8ccdfd
Manoj kumar Lekkalapudi <manojkumarlekkalapudi@Manojs-MacBook-Pro.local>
2017-12-26 15:54:28 -0500

    InitailCommit1

---
+++ src/main/java/com/geeks/spring/basics/SpringIn5Minutes/SpringIn5MinutesApplication.java
@@ -0,0 +1,12 @@
+package com.geeks.spring.basics.SpringIn5Minutes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIn5MinutesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIn5MinutesApplication.class, args);
+	}
+}
